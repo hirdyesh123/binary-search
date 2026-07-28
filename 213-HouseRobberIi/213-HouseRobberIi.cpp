b2d7eb13c011edbcf4bfce66a154358295115ff8
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:57:42 +0530

    Create 213-HouseRobberIi

---
+++ 213-HouseRobberIi/213-HouseRobberIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/28/2026, 9:57:42 PM
+class Solution {
+public:
+      int helper(int i,vector<int>&dp,int n,vector<int>&nums){
+         if(i>n) return 0;
+         if(dp[i]!=-1) return dp[i];
+        int pick=nums[i]+helper(i+2,dp,n,nums);
+        int notpick=helper(i+1,dp,n,nums);
+
+         return dp[i]=max(pick,notpick);  
+    }
+    int rob(vector<int>& nums) {
+           int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        vector<int>dp(nums.size(),-1);
+        vector<int>dp1(nums.size(),-1);
+     
+       int a= helper(0,dp,n-2,nums);
+       int b=helper(1,dp1,n-1,nums);
+       return max(a,b);
+    }
+};
