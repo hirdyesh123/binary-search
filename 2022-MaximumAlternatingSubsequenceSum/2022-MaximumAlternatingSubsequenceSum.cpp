052b59326b73b539a0a263ee713cd7054e1ad530
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:55:17 +0530

    Create 2022-MaximumAlternatingSubsequenceSum

---
+++ 2022-MaximumAlternatingSubsequenceSum/2022-MaximumAlternatingSubsequenceSum.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/28/2026, 9:55:17 PM
+class Solution {
+public:
+    long long helper(vector<int>&nums,int idx,vector<vector<long long>>&dp,bool pos){
+        if(idx>nums.size()-1) return 0;
+        if(dp[idx][pos]!=-1) return dp[idx][pos];
+
+        long long notpick=helper(nums,idx+1,dp,pos);
+        long long val=nums[idx];
+        if(pos==false) val=-val;
+        long long pick=val+helper(nums,idx+1,dp,!pos);
+        return dp[idx][pos]= max(pick,notpick);
+
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<long long>>dp(n,vector<long long>(2,-1));
+        return helper(nums,0,dp,true);
+    }
+};
