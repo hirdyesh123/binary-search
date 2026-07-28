b5b02f4de92b4b44ca84426b7c6251aa1e7ed94b
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:56:26 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/28/2026, 9:56:26 PM
+class Solution {
+public:
+    int numRescueBoats(vector<int>& nums, int limit) {
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right){
+            if(nums[left]+nums[right]>limit){
+                ans++;
+                right--;
+            }
+            else{
+                ans++;
+                left++;
+                right--;
+            }
+        }
+        return ans;
+    }
+};
