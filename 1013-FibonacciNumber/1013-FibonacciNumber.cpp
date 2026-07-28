d0f8daf5f9b1634292db00e6622ce07e3188ffdf
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:56:19 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/28/2026, 9:56:18 PM
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+           dp[i]=dp[i-1]+dp[i-2];
+        }
+       return dp[n];
+    }
+};
