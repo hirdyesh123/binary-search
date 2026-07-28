214a4633708cd057d4cd957e6f45804594a377a7
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:56:36 +0530

    Create 832-BinaryTreePruning

---
+++ 832-BinaryTreePruning/832-BinaryTreePruning.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/28/2026, 9:56:36 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int  helper(TreeNode* root){
+        if(root==NULL) return 0;
+        if(root->left==NULL && root->right==NULL){
+            if(root->val==1) return 1;
+            else return 0;
+        }
+        int left=helper(root->left);
+        int right=helper(root->right);
+        if(left==0) root->left=NULL;
+        if(right==0) root->right=NULL;
+        return left+right+root->val;
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+        helper(root);
+        if(root->val==0 && root->left==NULL && root->right==NULL) return NULL;
+        return root;
+    }
+};
