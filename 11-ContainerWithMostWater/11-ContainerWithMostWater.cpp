429f561d34595f6aec780f38567164ca4aae7c09
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:59:26 +0530

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/28/2026, 9:59:26 PM
+class Solution {
+public:
+    int maxArea(vector<int>& nums) {
+        int left=0;
+        int right=nums.size()-1;
+        int ans=0;
+
+        while(left<right){
+            int mult=right-left;
+            int water=min(nums[left],nums[right])*mult;
+            ans=max(ans,water);
+            
+            if(nums[left]<nums[right]) left++;
+            else right--;
+        }
+        return ans;
+    }
+};
