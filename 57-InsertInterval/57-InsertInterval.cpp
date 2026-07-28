995c12df7471dad77d0dd8cc88c894a0aaa05bf5
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:58:57 +0530

    Create 57-InsertInterval

---
+++ 57-InsertInterval/57-InsertInterval.cpp
@@ -0,0 +1,35 @@
+// Last updated: 7/28/2026, 9:58:57 PM
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newInterval) {
+        vector<vector<int>>ans;
+        bool inserted=false;
+        if(nums.size()==0) {
+            ans.push_back(newInterval);
+            return ans;
+        }
+        for(auto i:nums){
+            if(newInterval[0]<=i[1] && newInterval[0]>=i[0]){     //newnterval ko purana
+                newInterval[0]=min(newInterval[0],i[0]);
+                newInterval[1]=max(newInterval[1],i[1]);
+            } 
+            else if(i[0]<=newInterval[1] && i[0]>=newInterval[0]){
+                newInterval[0]=min(newInterval[0],i[0]);
+                newInterval[1]=max(newInterval[1],i[1]);
+            }  
+
+            else {
+               if(!inserted && newInterval[1] < i[0]){
+                ans.push_back(newInterval);
+                inserted=true;
+               }
+               ans.push_back(i);
+              
+            }  
+
+            
+        }
+         if(inserted==false) ans.push_back(newInterval);     
+        return ans;
+    }
+};
