e54316e916fe08133507fd4215cd6c7bd2918cc8
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:54:41 +0530

    Create 4216-WeightedWordMapping

---
+++ 4216-WeightedWordMapping/4216-WeightedWordMapping.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/28/2026, 9:54:41 PM
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans="";
+
+        for(auto i:words){
+            int sum=0;
+            for(auto j: i){
+               int temp=(int)j;
+               sum+=weights[temp-97];
+            }
+            int temp=sum%26;
+            char temp2=(char)(122-temp);
+            ans+=temp2;
+        }
+       return  ans;
+    }
+};
