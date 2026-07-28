d6daae5e84e72663585a7e11b0583ca1c1d233a6
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:58:23 +0530

    Create 112-PathSum

---
+++ 112-PathSum/112-PathSum.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/28/2026, 9:58:23 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root,int target,int currsum){
+        if(root==NULL) return false;
+        if(root->left==NULL && root->right==NULL){
+            currsum+=root->val;
+            if(currsum==target) return true;
+            else if(currsum<target) return false;
+        }
+       
+
+        bool left=helper(root->left,target,currsum+root->val);
+        bool right=helper(root->right,target,currsum+root->val);
+        return (left||right);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return helper(root,targetSum,0);
+    }
+};
