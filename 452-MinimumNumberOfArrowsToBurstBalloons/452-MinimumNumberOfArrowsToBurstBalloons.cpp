3d38eb5249c41defe005ffaf0b9c091bf6a89f3e
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:57:10 +0530

    Create 452-MinimumNumberOfArrowsToBurstBalloons

---
+++ 452-MinimumNumberOfArrowsToBurstBalloons/452-MinimumNumberOfArrowsToBurstBalloons.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/28/2026, 9:57:09 PM
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& nums) {
+        sort(nums.begin(),nums.end());
+        pair<int,int>current={nums[0][0],nums[0][1]};
+        int i=1;
+        int counter=1;
+        while(i<nums.size()){
+          int first=nums[i][0];
+          int second=nums[i][1];
+       
+          if(current.second>=first){
+             current.first=max(current.first,first);
+             current.second=min(current.second,second);
+          }
+          else {
+            counter++;
+            current={first,second};
+          }
+            i++;
+        }
+        return counter;
+    }
+};
