41c4a6a82bf0bdb62fa1b16f36a123e3f1e146a6
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:58:26 +0530

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/28/2026, 9:58:26 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+         int left= height(root->left);
+         int right=height(root->right);
+         if(abs(left-right)>1) return false;
+        return isBalanced(root->right) && isBalanced(root->left);
+      
+    }
+};
