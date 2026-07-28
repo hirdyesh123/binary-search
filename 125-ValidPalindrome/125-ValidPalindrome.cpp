1e9a44e39e9b4d201530684b072fbbacd11d6a5b
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:58:16 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/28/2026, 9:58:16 PM
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for (char &c : s) {
+           c = tolower(c);
+         }
+        int left=0;
+        int right=s.size();
+         while(left<right){
+           char ch1=s[left];
+           char ch2=s[right];
+           if( ! (( ch1>='a' && ch1<='z') || (ch1>='0'&& ch1<='9')) ) left++;
+           else if( ! (( ch2>='a' && ch2<='z') || (ch2>='0'&& ch2<='9')) ) right--;
+           else {
+                if(s[left]==s[right]) {
+                    left++;
+                    right--;
+                }
+                else return false;
+           }
+         }
+         return true;
+    }
+};
