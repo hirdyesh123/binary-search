8b9d43783216f499be1da3dd9e698504cef2c857
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:55:05 +0530

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/28/2026, 9:55:04 PM
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int posidx=0;
+        int negidx=1;
+        vector<int>ans(nums.size(),0);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+              ans[posidx]=nums[i];
+              posidx=posidx+2;
+            }
+            else {
+                ans[negidx]=nums[i];
+                negidx=negidx+2;
+            }
+        }
+        return ans;
+    }
+};
