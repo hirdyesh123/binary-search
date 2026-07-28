a26c31538510573b04468b4e19393162375c830f
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:55:56 +0530

    Create 1231-ReplaceElementsWithGreatestElementOnRightSide

---
+++ 1231-ReplaceElementsWithGreatestElementOnRightSide/1231-ReplaceElementsWithGreatestElementOnRightSide.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/28/2026, 9:55:56 PM
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& nums) {
+       // if(nums.size()==1) return {-1};
+        int maxe=nums[nums.size()-1];
+        nums[nums.size()-1]=-1;
+        for(int i=nums.size()-2;i>=0;i--){
+            int temp=nums[i];
+            nums[i]=max(nums[i+1],maxe);
+            maxe=max(maxe,temp);
+        }
+        return nums;
+    }
+};
