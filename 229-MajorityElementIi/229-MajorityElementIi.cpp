8b8a2836c175e4b3d19fef8b2028980440dd4b82
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:57:37 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/28/2026, 9:57:37 PM
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+       unordered_map<int,int>m;
+       vector<int>ans;
+       int n= nums.size();
+       for(int i=0;i<nums.size();i++){
+        m[nums[i]]++;
+       }
+       
+       for(auto it: m){
+          if( it.second > n/3) ans.push_back(it.first);
+       }
+       return ans;
+    }
+};
