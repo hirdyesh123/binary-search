655d8c099052f0225476d7ed0d6a53da048e7254
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:56:50 +0530

    Create 646-MaximumLengthOfPairChain

---
+++ 646-MaximumLengthOfPairChain/646-MaximumLengthOfPairChain.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/28/2026, 9:56:50 PM
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& nums) {
+        sort(nums.begin(), nums.end(),
+     [](vector<int>& a, vector<int>& b) {
+         return a[1] < b[1];
+     });
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            int len=1;
+            int temp=nums[i][1];
+            for(int j=i+1;j<nums.size();j++){
+               if(temp<nums[j][0]){
+                 temp=nums[j][1];
+                 len++;
+               }
+            }
+            ans=max(len,ans);
+        }
+        return ans;
+    }
+};
