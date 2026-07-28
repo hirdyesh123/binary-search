e37b8e046ce556dfe7bcedf78c22f811e81d0170
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:56:04 +0530

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/28/2026, 9:56:04 PM
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i=0;i<stones.size();i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+            int x= pq.top();
+            pq.pop();
+            int y= pq.top();
+            pq.pop();
+            pq.push(x-y);
+        }
+        return pq.top();
+    }
+};
