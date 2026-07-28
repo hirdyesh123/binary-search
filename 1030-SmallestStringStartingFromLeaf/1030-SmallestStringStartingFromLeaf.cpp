7a7ea425931e88b0124bd20ee317dfad1cd3d21f
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-28 21:56:13 +0530

    Create 1030-SmallestStringStartingFromLeaf

---
+++ 1030-SmallestStringStartingFromLeaf/1030-SmallestStringStartingFromLeaf.cpp
@@ -0,0 +1,34 @@
+// Last updated: 7/28/2026, 9:56:12 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string mini="";
+    void helper(TreeNode* root,string s){
+        if(root==NULL) return;
+        if(root->left==NULL && root->right==NULL){
+            s=char(root->val+97)+s;
+            if(mini=="") mini=s;
+            else mini=min(mini,s);
+            return;
+        }
+        s=char(root->val+97)+s;
+        helper(root->left,s);
+        helper(root->right,s);
+    }
+    string smallestFromLeaf(TreeNode* root) {
+        helper(root,"");
+
+
+        return  mini;
+    }
+};
