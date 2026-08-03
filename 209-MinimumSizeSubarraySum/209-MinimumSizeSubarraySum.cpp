707c02db49cd0b84783cf6b9dd3abcdfc5c053fd
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-03 11:39:11 +0530

    File updated at 8/3/2026, 11:39:11 AM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.cpp
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.cpp
@@ -1,21 +1,22 @@
-// Last updated: 7/28/2026, 9:57:45 PM
-class Solution {
-public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int l=0;
-        int r=0;
-        int sum=0;
-        int ans=INT_MAX;
-        while(r<nums.size()){
-            sum+=nums[r];
-            while(sum>=target){
-               ans=min(ans,r-l+1);
-               sum-=nums[l];
-               l++;
-            }
-              r++;
-        }
-        if(ans==INT_MAX) return 0;
-        return ans;
-    }
-};
+// Last updated: 8/3/2026, 11:39:11 AM
+1class Solution {
+2public:
+3    int minSubArrayLen(int target, vector<int>& nums) {
+4        int ans=INT_MAX;
+5        int i=0;
+6        int j=0;
+7        int sum=0;
+8        while(j<nums.size()){
+9            sum=sum+nums[j];
+10            if(sum>=target) ans=min(ans,j-i+1);
+11            while(sum>=target){
+12                ans=min(ans,j-i+1);
+13                sum=sum-nums[i];
+14                i++;
+15            }
+16            j++;
+17        }
+18        if(ans==INT_MAX) return 0;
+19        return ans;
+20    }
+21};
