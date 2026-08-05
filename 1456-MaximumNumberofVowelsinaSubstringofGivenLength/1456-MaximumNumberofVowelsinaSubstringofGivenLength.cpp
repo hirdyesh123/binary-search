66e40727f7905f176d4f7885e9e35186e85141bf
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-05 13:44:25 +0530

    File updated at 8/5/2026, 1:44:25 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
@@ -1,27 +1,19 @@
-// Last updated: 8/5/2026, 11:50:43 AM
+// Last updated: 8/5/2026, 1:44:25 PM
 1class Solution {
 2public:
-3    void helper(deque<int>&dq,int idx,vector<int>&nums){
-4        while(!dq.empty() && nums[idx]>nums[dq.back()]) dq.pop_back();
-5        dq.push_back(idx);
-6    }
-7    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-8        int left=0;
-9        int right=0;
-10        deque<int>dq;
-11        vector<int>ans;
-12        while(right<nums.size()){
-13            helper(dq,right,nums);
-14           
-15            while(right-left+1==k){
-16               ans.push_back(nums[dq.front()]);
-17               if(nums[dq.front()]==nums[left]) dq.pop_front();
-18               left++;
-19            }
-20
-21            right++;
-22        }
-23
-24        return ans;
-25    }
-26};
+3    int numSubarraysWithSum(vector<int>& nums, int goal) {
+4        unordered_map<int,int>map;  //key freq
+5        vector<int>prefix(nums.size()+1,0);
+6        int ans=0;
+7        for(int i=1;i<prefix.size();i++){
+8          prefix[i]=nums[i-1]+prefix[i-1];
+9        }
+10        
+11        for(int i=0;i<prefix.size();i++){
+12            if(map.find(prefix[i]-goal)!=map.end()) ans=ans+map[prefix[i]-goal];
+13
+14            map[prefix[i]]++;
+15        }
+16        return ans;
+17    }
+18};
