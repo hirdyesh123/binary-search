b7dd44a8ab14bb7c53deb9edd46435b35f2ae049
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-05 14:18:26 +0530

    File updated at 8/5/2026, 2:18:25 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
@@ -1,21 +1,28 @@
-// Last updated: 8/5/2026, 2:01:40 PM
+// Last updated: 8/5/2026, 2:18:25 PM
 1class Solution {
 2public:
-3    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-4        int i=0;
-5        int j=0;
-6        if(k<=1) return 0;
-7        long long mult=1;
-8        int ans=0;
-9        while(j<nums.size()){
-10            mult=mult*nums[j];
-11           while (mult>=k){
-12             mult=mult/nums[i];
-13             i++;
-14           }
-15          if(i<nums.size())  ans+=j-i+1;
-16           j++;
-17        }
-18        return ans;
-19    }
-20};
+3    bool check(unordered_map<int,int>&map,int k){
+4        for(auto &i:map){
+5            if(i.second>k) return false;
+6        }
+7        return true;
+8    }
+9    int maxSubarrayLength(vector<int>& nums, int k) {
+10        int ans=0;
+11        int i=0;
+12        int j=0;
+13        unordered_map<int,int>map;
+14        while(j<nums.size()){
+15            map[nums[j]]++;
+16            if(map[nums[j]]<=k) ans=max(ans,j-i+1);
+17            else {
+18                while(i<j && map[nums[j]]>k){
+19                    map[nums[i]]--;
+20                    i++;
+21                }
+22            }
+23            j++;
+24        }
+25        return ans;
+26    }
+27};
