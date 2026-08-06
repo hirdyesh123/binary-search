323787829099f855fc7aa6af86219984c0ae02e2
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-06 12:31:31 +0530

    File updated at 8/6/2026, 12:31:31 PM

--- 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.cpp
+++ 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.cpp
@@ -1,20 +1,26 @@
-// Last updated: 8/6/2026, 11:55:57 AM
+// Last updated: 8/6/2026, 12:31:31 PM
 1class Solution {
 2public:
-3    int equalSubstring(string s, string t, int maxcost) {
-4        int i=0;  //use for traversing in t;
-5        int j=0;  //use for traversing in t;
-6        int currcost=0;
-7        int ans=0;
-8        while(j<t.size()){
-9            currcost+=abs(s[j]-t[j]);
-10            while(currcost>maxcost){
-11                currcost-=abs(s[i]-t[i]);
-12                i++;
-13            }
-14            ans=max(ans,j-i+1);
-15            j++;
-16        }
-17        return ans;
-18    }
-19};
+3    int maxSatisfied(vector<int>& nums, vector<int>& grumpy, int k) {
+4        int total=0;
+5        int sum=0;
+6        int ans=0;
+7        for(int i=0;i<nums.size();i++){
+8            if(grumpy[i]==0) total+=nums[i];
+9        }
+10        int i=0;
+11        int j=0;
+12        while(j<nums.size()){
+13           if(grumpy[j]==1) sum=sum+nums[j];
+14           
+15           
+16           if(j-i+1>k){
+17              if(grumpy[i]==1) sum=sum-nums[i];
+18              i++;
+19           }
+20           if(j-i+1==k) ans=max(ans,total+sum);
+21           j++;
+22        }
+23        return ans;
+24    }
+25};
