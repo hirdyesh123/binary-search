c18305618583b2e4b24a4cad29443531e71d2a36
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-08 00:13:20 +0530

    File updated at 8/8/2026, 12:13:20 AM

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.cpp
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/8/2026, 12:10:37 AM
+// Last updated: 8/8/2026, 12:13:20 AM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -13,19 +13,22 @@
 12class Solution {
 13public:
 14    priority_queue<int,vector<int>,greater<int>>pq;
-15    void helper(TreeNode* root){
-16        if(root==NULL) return ;
-17        pq.push(root->val);
-18        helper(root->left);
-19        helper(root->right);
-20    }
-21    int kthSmallest(TreeNode* root, int k) {
-22        int ans=0;
-23        helper(root);
-24        while(!pq.empty() && k>0){
-25             ans=pq.top();
-26            pq.pop();k--;
-27        }
-28        return ans;
-29    }
-30};
+15    int temp;
+16    void helper(TreeNode* root){
+17        if(root==NULL) return ;
+18        pq.push(root->val);
+19        helper(root->left);
+20       
+21        helper(root->right);
+22    }
+23    int kthSmallest(TreeNode* root, int k) {
+24        temp=k;
+25        int ans=0;
+26        helper(root);
+27        while(!pq.empty() && k>0){
+28             ans=pq.top();
+29            pq.pop();k--;
+30        }
+31        return ans;
+32    }
+33};
