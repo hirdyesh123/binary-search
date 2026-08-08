6a3ef2db6b7c252db4a9cd106a40cb949cd7b799
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-08 11:38:21 +0530

    File updated at 8/8/2026, 1:59:45 AM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
@@ -1,19 +1,23 @@
-// Last updated: 8/8/2026, 1:37:28 AM
+// Last updated: 8/8/2026, 1:59:45 AM
 1class Solution {
 2public:
-3    int kthSmallest(vector<vector<int>>& nums, int k) {
-4        priority_queue<int,vector<int>,greater<int>>pq;
-5        int ans=0;
-6        for(int i=0;i<nums.size();i++){
-7            for(int j=0;j<nums.size();j++){
-8                pq.push(nums[i][j]);
-9            }
+3    int halveArray(vector<int>& nums) {
+4        double sum=0;
+5        int count=0;
+6        priority_queue<double>pq;
+7        for(int i=0;i<nums.size();i++){
+8            sum+=nums[i];
+9            pq.push(nums[i]);
 10        }
-11
-12        while(k--){
-13            ans=pq.top();
-14            pq.pop();
-15        }
-16        return ans;
-17    }
-18};
+11        double currsum=sum;
+12        sum=sum/2;
+13       while(!pq.empty() && currsum>sum){
+14         double front=pq.top();
+15         pq.pop();
+16         currsum=currsum-front/2;
+17         pq.push(front/2);
+18         count++;
+19       }
+20       return  count;
+21    }
+22};
