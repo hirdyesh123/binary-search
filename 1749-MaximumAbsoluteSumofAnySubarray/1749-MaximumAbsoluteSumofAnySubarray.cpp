c948c44d900893b25342036d2763e0b04f75cce7
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-08-19 11:58:47 +0530

    File updated at 8/19/2026, 11:58:47 AM

--- 1749-MaximumAbsoluteSumofAnySubarray/1749-MaximumAbsoluteSumofAnySubarray.cpp
+++ 1749-MaximumAbsoluteSumofAnySubarray/1749-MaximumAbsoluteSumofAnySubarray.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/19/2026, 11:58:25 AM
+// Last updated: 8/19/2026, 11:58:47 AM
 1class Solution {
 2public:
 3    int maxAbsoluteSum(vector<int>& nums) {
@@ -6,18 +6,17 @@
 5        int sum=0;
 6        for(int i=0;i<nums.size();i++){
 7            sum=sum+nums[i];
-8            maxsum=max(sum,maxsum);
-9            if(sum<0) sum=0;
-10        }
-11        for(int i=0;i<nums.size();i++){
-12            nums[i]=nums[i]*-1;
-13        }
-14        sum=0;
-15        for(int i=0;i<nums.size();i++){
-16             sum=sum+nums[i];
-17            maxsum=max(sum,maxsum);
-18            if(sum<0) sum=0;
-19        }
-20        return maxsum;
-21    }
-22};
+8            nums[i]=nums[i]*-1;
+9            maxsum=max(sum,maxsum);
+10            if(sum<0) sum=0;
+11        }
+12       
+13        sum=0;
+14        for(int i=0;i<nums.size();i++){
+15             sum=sum+nums[i];
+16            maxsum=max(sum,maxsum);
+17            if(sum<0) sum=0;
+18        }
+19        return maxsum;
+20    }
+21};
