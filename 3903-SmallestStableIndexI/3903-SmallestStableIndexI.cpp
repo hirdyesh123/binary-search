82754957fca0f39695fa46bf5c1ebaab92cd172d
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-09-04 22:03:30 +0530

    Create 3903-SmallestStableIndexI

---
+++ 3903-SmallestStableIndexI/3903-SmallestStableIndexI.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/4/2026, 10:03:29 PM
+1class Solution {
+2public:
+3    int firstStableIndex(vector<int>& nums, int k) {
+4        vector<int>x(nums.size(),0);
+5        pair<bool,int>ans={false,nums.size()-1};
+6        x[nums.size()-1]=nums[nums.size()-1];
+7        for(int i=nums.size()-2;i>=0;i--){
+8            x[i]=min(x[i+1],nums[i]);
+9        }
+10        int maxe=INT_MIN;
+11        int idx=-1;
+12        for(int i=0;i<nums.size();i++){
+13            maxe=max(maxe,nums[i]);
+14           int check=maxe-x[i];
+15           if(check<=k ){
+16            ans.first=true;
+17            ans.second=min(ans.second,i);
+18           }
+19        }
+20        if(!ans.first) return -1;
+21        return ans.second;
+22    }
+23}; 
