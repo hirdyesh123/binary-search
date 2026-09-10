8a5087dc330ef72f1e8f8de5dcf156e561c2d4ca
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-09-10 12:25:14 +0530

    File updated at 9/10/2026, 12:25:14 PM

--- 735-AsteroidCollision/735-AsteroidCollision.cpp
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -1,25 +1,41 @@
-// Last updated: 7/28/2026, 9:56:44 PM
-class Solution {
-public:
-    vector<int> asteroidCollision(vector<int>& nums) {
-        stack<int>st;
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>0) st.push(nums[i]);
-            else {
-            
-                while (!st.empty() && abs(nums[i]) > abs(st.top()) && st.top() > 0)  st.pop();
-                if(!st.empty() && st.top()==abs(nums[i])) st.pop();
-                 else if(st.empty() || st.top()<0) st.push(nums[i]);
-             }
-            
-        }
-        while(st.size()>0){
-            int x=st.top();
-            ans.push_back(x);
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    }
-};
+// Last updated: 9/10/2026, 12:25:14 PM
+1class Solution {
+2public:
+3    bool check(stack<int>& st, int x) {
+4    return st.top() > 0 && x < 0;
+5}
+6    vector<int> helper(vector<int>&ans,stack<int>&st){
+7        while(!st.empty()){
+8            ans.push_back(st.top());
+9            st.pop();
+10        }
+11       reverse(ans.begin(),ans.end());
+12       return ans;
+13    }
+14    vector<int> asteroidCollision(vector<int>& nums) {
+15         stack<int>st;
+16         for(int i=0;i<nums.size();i++){
+17            if(st.empty()) st.push(nums[i]);
+18            else {
+19                while(!st.empty() && check(st,nums[i]) ){
+20                    if(abs(st.top())<abs(nums[i])) {
+21                        st.pop();
+22                    }
+23                    else if(abs(st.top())==abs(nums[i])){
+24                        st.pop();
+25                        nums[i]=0;
+26                        break;
+27                    }
+28                    else {
+29                       
+30                        nums[i]=0;
+31                          break;
+32                    }
+33                }
+34               if(nums[i]!=0) st.push(nums[i]);
+35            }
+36         }
+37         vector<int>ans;
+38         return helper(ans,st);
+39    }
+40};
