f5b0d2fdfead1098c99b6f583ff0e5fb83a69fa0
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-09-15 11:22:08 +0530

    File updated at 9/15/2026, 11:22:08 AM

--- 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.cpp
+++ 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.cpp
@@ -1,19 +1,25 @@
-// Last updated: 9/15/2026, 11:08:29 AM
+// Last updated: 9/15/2026, 11:22:08 AM
 1class Solution {
 2public:
-3    vector<string> buildArray(vector<int>& target, int n) {
-4        vector<string>ans;
-5        unordered_set<int>st;
-6        for(auto i:target){
-7            st.insert(i);
-8        }
-9        for(int i=1;i<=*max_element(target.begin(),target.end());i++){
-10            if(st.find(i)!=st.end()) ans.push_back("Push");
-11            else {
-12                ans.push_back("Push");
-13                ans.push_back("Pop");
-14            }
-15        }
-16        return ans;
-17    }
-18};
+3    string minRemoveToMakeValid(string s) {
+4        vector<bool>flag(s.size(),false);
+5        stack<pair<char,int>>st;  //char,index
+6        string ans="";
+7        for(int i=0;i<s.size();i++){
+8            if(s[i]==')' || s[i]=='('){
+9                if(!st.empty() && st.top().first=='(' && s[i]==')') st.pop();
+10                else st.push({s[i],i});
+11            }
+12            else continue;
+13        }
+14        while(!st.empty()){
+15            auto x=st.top();
+16            st.pop();
+17            flag[x.second]=true;
+18        }
+19        for(int i=0;i<flag.size();i++){
+20            if(flag[i]==false) ans.push_back(s[i]);
+21        }
+22        return ans;
+23    }
+24};
